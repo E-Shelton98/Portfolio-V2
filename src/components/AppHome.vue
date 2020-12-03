8dd5eeb800dd02e3fc837eaac359451cec1ba9c1
Erik Shelton <erikshelton@Eriks-Air.lan1>
2020-12-02 21:21:49 -0500

    info card skeleton created

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -4,7 +4,7 @@
             <div class="border border-gray-400 h-32 w-32 my-4 rounded-full">
 
             </div>
-            <div class="border border-gray-400 h-24 mx-4">
+            <div class="h-24 mx-4">
                 <section>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam consectetur hic rerum atque ratione debitis, nulla mollitia at quo optio a ducimus dignissimos saepe, laborum ea. Enim ratione nulla vero.</section>
             </div>
             <div class="mt-16 mx-1">
@@ -13,6 +13,12 @@
                 <button class="btn"></button>
                 <button class="btn"></button>
             </div>
+        </div>
+        <div class="border border-gray-400 h-auto m-8">
+            <div class="h-auto m-4">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda ipsam pariatur vero deserunt dolorem rem repellat. Ipsam corporis sit corrupti excepturi aut commodi. Necessitatibus repellat sapiente magni nobis ipsa consequatur? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quisquam, repellat harum esse dolores perferendis minus quo tempore est illo dolor corrupti commodi eum fugit fugiat voluptates. Exercitationem molestiae libero accusamus. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Est iure ipsum repellendus dolor enim, id mollitia consectetur excepturi, doloribus sequi voluptate ratione! Magnam nulla labore repellat, repellendus eaque veritatis eum.</div>
+        </div>
+        <div class="border-2 border-red-400 h-96 m-4">
+            <section class="text-center">Projects</section>
         </div>
     </div>
 </template>
@@ -25,6 +31,6 @@ export default {
 
 <style>
     .btn {
-        @apply border border-gray-400 h-8 w-24 m-2 rounded-md
+        @apply border border-gray-400 h-8 w-24 m-1 rounded-md
     }
 </style>
