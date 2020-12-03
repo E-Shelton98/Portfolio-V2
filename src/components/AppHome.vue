de69e860a2b6c8f5778cd3758f808acbb6accbb7
Erik Shelton <erikshelton@Eriks-Air.lan1>
2020-12-02 21:14:58 -0500

    AppHome setup, created head card skeleton

---
+++ src/components/AppHome.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <div class="border border-gray-400 h-96 m-4 flex flex-col items-center">
+            <div class="border border-gray-400 h-32 w-32 my-4 rounded-full">
+
+            </div>
+            <div class="border border-gray-400 h-24 mx-4">
+                <section>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam consectetur hic rerum atque ratione debitis, nulla mollitia at quo optio a ducimus dignissimos saepe, laborum ea. Enim ratione nulla vero.</section>
+            </div>
+            <div class="mt-16 mx-1">
+                <button class="btn"></button>
+                <button class="btn"></button>
+                <button class="btn"></button>
+                <button class="btn"></button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'AppHome'
+}
+</script>
+
+<style>
+    .btn {
+        @apply border border-gray-400 h-8 w-24 m-2 rounded-md
+    }
+</style>
